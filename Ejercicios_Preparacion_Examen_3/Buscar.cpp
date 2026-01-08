e1073e88d650d06b32dcbb0dced4d2ee6f96140b
Óscar <oscar09gmail.es>
2026-01-08 13:01:48 +0100

    Ejercicio buscar

---
+++ Ejercicios_Preparacion_Examen_3/Buscar.cpp
@@ -0,0 +1,11 @@
+
+/*Funcion buscar() que reciba dos textos, palabra y letras,
+y devuelva el numero de casilla de palabra donde por primera
+vez aparece una letra de letras. Los textos son un array basico de caracteres. 
+Ejemplo: si palabra es el array "" que equivale a 
+"Las rosas son rojas"; y letras es "" que equivale a "mjnv",
+entonces la funcion devuelve 12*/
+
+/*Otro ejemplo mas facil:
+       {H A L O}
+letras {A O}      ESTO DEVUELVE 1  */
